404808359a03045298584d351a2730205a6a3899
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-10-15 14:10:00 +0545

    added frontend

--- non-psql/script.js
+++ non-psql/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-alert("ok!");
+	var QUERY_URL = 'http://45.55.41.12:8000/script.php'
+	var SERVER_URL = 'http://45.55.41.12:8000/'
+	var fromDate = '';
+	var toDate = ''
+	var formType = '';
+	new Pikaday({
+		field: $('#from_datepicker')[0],
+		format: 'YYYY-MM-DD',
+		onSelect: function() {
+			fromDate = moment(this._d).format('YYYY-MM-DD');
+		}
+	});
+	new Pikaday({
+		field: $('#to_datepicker')[0],
+		format: 'YYYY-MM-DD',
+		onSelect: function() {
+			toDate = moment(this._d).format('YYYY-MM-DD');
+		}
+	});
+
+	$('#submit_btn').click(function(e) {
+		var formType = $('.form-type')[0];
+		formType = formType.options[formType.selectedIndex].value;
+		var query = QUERY_URL + '?startdate=' + fromDate + '&enddate=' +
+								toDate + '&tablename=' + formType;
+		console.log(query);
+		$.get(query, function(data) {
+			console.log(data);
+			var zipName = data.split(' ')[0];
+			$('<a href="' + SERVER_URL + zipName + '">Download Zipped ' + 
+					zipName.split('_')[0].toUpperCase() + 'data</a>').appendTo('.container');
+		});
+	});
 });
--- non-psql/script.php
+++ non-psql/script.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
 $startDate = $_GET['startdate'];
 $endDate = $_GET['enddate'];
@@ -7,7 +8,8 @@ $tableIDs = file_get_contents("tableIDs.json");
 $tableIDs = json_decode($tableIDs, true);
 
 //&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $enddate, $results)
-if(preg_match("/(\d{4})-(\d{2})-(\d{2})/", $startDate, $results) && preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results) && $tableIDs[$tableName]){
+if(preg_match("/(\d{4})-(\d{2})-(\d{2})/", $startDate, $results) 
+	&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results) && $tableIDs[$tableName]){
 	echo exec('./ona-get-data-and-run-extractor.sh '.$startDate.' '.$endDate.' '.$tableName.' '.$tableIDs[$tableName]);
 }else{
 	echo "no";

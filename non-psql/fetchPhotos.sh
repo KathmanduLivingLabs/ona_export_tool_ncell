fe25a6ce1a9fd132543096ce97498ebfdf1a8776
root <gautamxpratik@gmail.com>
2015-10-16 03:31:20 -0400

    file script

--- non-psql/fetchPhotos.sh
+++ non-psql/fetchPhotos.sh
@@ -1,4 +1,4 @@
-cd /root/jedi-Knight/ona-data-date-filtered-archives/non-psql
+cd /var/www/sida/ona-data-date-filtered-archives/non-psql
 aws s3 cp s3://onadata/wbsida321/attachments/ ./ --recursive --exclude "*" --include "*-large.jpg"
 rename 's/-large.jpg/.jpg/' *.jpg
 

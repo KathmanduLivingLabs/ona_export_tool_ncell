f03db7d0a0cf2781118c23707cbc398128d10feb
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-28 12:19:34 +0545

    ziplist from directcsv.js

--- non-psql/script.php
+++ non-psql/script.php
@@ -10,7 +10,7 @@ $tableIDs = json_decode($tableIDs, true);
 //&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $enddate, $results)
 if(preg_match("/(\d{4})-(\d{2})-(\d{2})/", $startDate, $results) 
 	&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results) && $tableIDs[$tableName]){
-	echo exec('./ona-get-data-and-run-extractor.sh '.$startDate.' '.$endDate.' '.$tableName.' '.$tableIDs[$tableName]);
+	echo explode(" ", exec('./ona-get-data-and-run-extractor.sh '.$startDate.' '.$endDate.' '.$tableName.' '.$tableIDs[$tableName]))[0];
 }else{
 	echo "no";
 }

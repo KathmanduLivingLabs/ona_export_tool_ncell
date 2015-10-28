1092937899ad2e5e2d2a15e0bce935e186bc4569
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-28 13:09:33 +0545

    ziplist from directcsv.js

--- non-psql/zipList.sh
+++ non-psql/zipList.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 zipList=$(./ona-extract-from-csv-by-date.js $1 $2 $3)
 zip $zipList>dump
-#a="$(stat --printf="%s" ${zipList[0]})"
-IFS=' ' read -a filenames <<< "${zipList[0]}"
-a="$(stat --printf="%s" ${filenames[1]})"
-echo ${filenames[1]}
-if[ "$a" == "0"]
-if (( a == 0 ))
-then
-	echo "0"
-else
-	echo ${zipList[0]}
-fi
+echo ${zipList[0]}

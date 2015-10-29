7ceb95d79da1733e6c47c19ce979cc392ef9c3bc
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-29 16:37:40 +0545

    client-requests-processor

---
+++ non-psql/index.php
@@ -0,0 +1,24 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header('Content-Type: text/plain');
+
+	$emis = $_GET['emis'];
+	$group = $_GET['group'];
+
+	if(!($emis && $group)){
+		echo exec('./ona-list-emis.sh');
+	}else{
+		if($group=='buildings'){
+			$data = file_get_contents('buildings.json');
+			//echo $emis;
+			preg_match_all('/'.$emis.'-./', $data, $matches);
+			//echo $data;
+			echo join(",", $matches[0]);
+		}else{
+			$data = file_get_contents('building_elements.json');
+			//echo $data;
+			preg_match_all('/'.$emis.'-\\w+-\\d+/', $data, $matches);
+			echo join(",", $matches[0]);
+		}
+	}
+?>

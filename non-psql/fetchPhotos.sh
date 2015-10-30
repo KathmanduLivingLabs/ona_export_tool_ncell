f5f29012896b02101cd01cf725a23f3a5800538b
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-31 01:34:56 +0545

    revert later

--- non-psql/fetchPhotos.sh
+++ non-psql/fetchPhotos.sh
@@ -3,7 +3,7 @@
 #aws s3 sync s3://onadata/wbsida321/attachments/ ./ --exclude "*" --include "*-large.jpg" >photolog.txt
 #aws s3 sync s3://onadata/wbsida321/attachments/ ./ --exclude "*" --include "*-large.jpg" >>photolog.txt
 
-aws s3 ls s3://onadata/wbsida321/attachments/ > filelist.txt
+#aws s3 ls s3://onadata/wbsida321/attachments/ > filelist.txt
 
 array=$(cat filelist.txt | grep -o "[0-9]\{3,13\}.jpg")
 

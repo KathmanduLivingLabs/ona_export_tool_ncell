6367f466eda4af87dad99e366bad077846d6ad0f
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-30 12:01:30 +0545

    arraylist

--- non-psql/fetchPhotos.sh
+++ non-psql/fetchPhotos.sh
@@ -5,7 +5,9 @@
 
 aws s3 ls s3://onadata/wbsida321/attachments/ > filelist.txt
 
-array=$(cat filelist.txt | grep -o "[0-9]\{13\}.jpg")
+array=$(cat filelist.txt | grep -o "[0-9]\{3,13\}.jpg")
+
+echo $array>arraylist.txt
 
 for i in "${array[@]}"
 do

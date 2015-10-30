8775915e7fa0a1a3c2b3e014a914d83e5a9ef82b
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-30 08:50:59 +0545

    aws debug

--- non-psql/fetchPhotos.sh
+++ non-psql/fetchPhotos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 sync s3://onadata/wbsida321/attachments/ ./ --recursive --exclude "*" --include "*-large.jpg" >photolog.txt
-aws s3 sync s3://onadata/wbsida321/attachments/ ./ --recursive --exclude "*" --include "*-large.jpg" >>photolog.txt
+aws s3 sync s3://onadata/wbsida321/attachments/ ./ --exclude "*" --include "*-large.jpg" >photolog.txt
+aws s3 sync s3://onadata/wbsida321/attachments/ ./ --exclude "*" --include "*-large.jpg" >>photolog.txt
 

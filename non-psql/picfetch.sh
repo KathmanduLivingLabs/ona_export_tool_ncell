2f05e33b4e2ef63076a169c24ae9dc8a7464dc78
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-30 12:35:54 +0545

    picfetch

--- non-psql/picfetch.sh
+++ non-psql/picfetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 cp s3://onadata/wbsida321/attachments/$1 ./
-mogrify -resize 25% $1
-rename 's/.jpg/-large.jpg/' $1.jpg
+aws s3 cp s3://onadata/wbsida321/attachments/$1 ./ >>awslog.log
+mogrify -resize 25% $1 >>morgifylog.log
+rename 's/.jpg/-large.jpg/' $1

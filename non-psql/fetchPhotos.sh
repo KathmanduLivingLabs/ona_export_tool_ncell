98a34d0fb3980f886805366bf06dc2e007176e5d
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-31 02:11:22 +0545

    newpiclist

--- non-psql/fetchPhotos.sh
+++ non-psql/fetchPhotos.sh
@@ -9,6 +9,6 @@ array=$(cat filelist.txt | grep -o "[0-9]\{3,13\}.jpg")
 
 echo $array>arraylist.src.txt
 
-(ls *.jpg | grep -oc "[0-9]\{3,13\}-large.jpg") | Rscript newpiclist.r > arraylist.txt
+(ls *.jpg | grep -o "[0-9]\{3,13\}-large.jpg" | perl -p -e "s/-large//g") | Rscript newpiclist.r > arraylist.txt
 
 php picfetch.php

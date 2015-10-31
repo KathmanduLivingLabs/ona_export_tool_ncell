b8874eb88ea9d6a038b49f162760c3e481168bca
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-31 17:35:05 +0545

    .sh

--- non-psql/picfetch.sh
+++ non-psql/picfetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 s3cmd get s3://onadata/wbsida321/attachments/$1 > /dev/null || { echo "error downloading file $1" >s3errors.log; }
-./picscale 4 $1
+./picscale.sh 4 $1
 rename 's/.jpg/-large.jpg/' $1

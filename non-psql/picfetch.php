cafd23b893001fce551676b554bbe40971939c89
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-31 18:25:50 +0545

    picfetch

--- non-psql/picfetch.php
+++ non-psql/picfetch.php
@@ -3,9 +3,22 @@
 $arr = file_get_contents("arraylist.txt");
 $arr = explode(" ", $arr);
 
+$total = count($arr);
+
+echo "Files to download = ".$t;
+
+$starttime = time();
+
 foreach($arr as $k=>$i){
 	exec("./picfetch.sh ".$i);
-	if(!($k%500)) echo $k;
+	if(!($k%500)) {
+		$timeelapsed = (time()-$starttime)/60;
+		echo $k."/".$t." files downloaded..Time elapsed: ". $timeelapsed." minutes..\n";
+	}
 }
+
+$timeelapsed = (time()-$starttime)/60;
+
+echo "All files downloaded! Total time taken for operation: ".$timeelapsed." minutes.\n";
 
 ?>

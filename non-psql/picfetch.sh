9817592e04be2e195f40b70efda807c253f2fd5f
Pratik Gautam <gautamxpratik@gmail.com>
2015-10-31 17:30:55 +0545

    what is this error?

--- non-psql/picfetch.sh
+++ non-psql/picfetch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#aws s3 cp s3://onadata/wbsida321/attachments/$1 ./ >>awslog.log
-#mogrify -resize 25% $1 >>morgifylog.log
 s3cmd get s3://onadata/wbsida321/attachments/$1 > /dev/null || { echo "error downloading file $1" >s3errors.log }
-./picscale 4 $1 || { echo "error downloading file $1" >picscaleerrors.log }
+./picscale 4 $1
 rename 's/.jpg/-large.jpg/' $1

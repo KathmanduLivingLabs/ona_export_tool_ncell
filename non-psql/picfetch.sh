45f53215335fcfddc5b9694b6f6d89deafe69747
root <gautamxpratik@gmail.com>
2015-11-01 00:07:25 -0400

    picfetch

--- non-psql/picfetch.sh
+++ non-psql/picfetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-s3cmd get s3://onadata/wbsida321/attachments/$1 > /dev/null || { echo "error downloading file $1" >s3errors.log; }
-./picscale.sh 4 $1
-rename 's/.jpg/-large.jpg/' $1
+s3cmd get s3://onadata/wbsida321/attachments/$1 > /dev/null || { echo "error downloading file $1" >>s3errors.log; }
+./picscale.sh 4 $1  || { echo "error downloading file $1" >>picscalesherrors.log; }
+rename 's/.jpg/-large.jpg/' $1 || { echo "error downloading file $1" >>renameerrors.log; }

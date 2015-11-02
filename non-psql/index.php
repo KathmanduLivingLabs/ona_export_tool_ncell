a0c1a02a5e7da7ca0b6f0150247acc748c2972db
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-02 23:38:09 +0545

    wkpdftohtml;

--- non-psql/index.php
+++ non-psql/index.php
@@ -37,7 +37,7 @@
 			//$genDoc = array_merge($matches);
 		//}else{
 			//$emis = preg_replace('/(EMIS)(\\d{2})/', "$1$2$2", $emis);
-			exec("./htmlgen.sh $emis building_elements");
+			exec("./htmlgen.sh ".preg_replace('/(EMIS)(\\d{2})/', "$1$2$2", $emis);." building_elements");
 
 			//$data = file_get_contents('building_elements.json');
 			//echo $data;
@@ -45,33 +45,38 @@
 			//$genDoc = array_merge($matches);
 			//echo join(",", $matches2[0]);
 
-			$genDocList = explode(",",preg_replace('/.html/ui', "", exec("ls output/ | grep $emis*.html | cut -d, -f2- | paste -sd,")));
+			$genDocList = explode(",",preg_replace('/.html/ui', "", exec("ls output/ | grep $emis | grep html  | cut -d, -f2- | paste -sd,")));
 			asort($genDocList);
 			$baseFileName = array_pop($genDocList);
 			array_unshift($genDocList, $baseFileName);
 
-			$tocml = "<html><head></head><body><style></style><h1>Table of Contents</h1>";
+			$tocml = "<html><head></head><body><style>h4{display: inline-block; margin-left: 20px;}</style><h1>Table of Contents</h1>";
 			$tocFileName = $baseFileName."-toc";
+
+			$numPages = 1;
 
 			foreach($genDocList as $docName){
 
-				exec("html-pdf output/$docName.html output/$docName.pdf");
+				//exec("html-pdf output/$docName.html output/$docName.pdf");
+				exec('xvfb-run --server-args="-screen 0, 1366x768x24" wkhtmltopdf output/$docName.html _temp/$docName.pdf');
 
-				$numPages = intval(exec("pdfinfo output/$docName.pdf | grep Pages | awk '{print $2}'"));
+				$numPages += intval(exec("pdfinfo output/$docName.pdf | grep Pages | awk '{print $2}'"));
 
 				$tocml .= "<div><h4>".str_replace(".html.pdf", "", $docName)."</h4>  ................  <h4>".$numPages."</h4></div>";
 			}
 
 			$tocml .= "</body></html>";
 
-			file_put_contents("output/$tocFileName.html", $tocml);
-			exec("html-pdf output/$tocFileName.html output/$tocFileName");
+			file_put_contents("_temp/$tocFileName.html", $tocml);
+			//exec("html-pdf _temp/$tocFileName.html _temp/$tocFileName.pdf");
+			exec('xvfb-run --server-args="-screen 0, 1366x768x24" wkhtmltopdf _temp/$tocFileName.html _temp/$tocFileName');
 
 			$genDocList = $tocFileName.".pdf ".implode(".pdf ", $genDocList).".pdf $emis-compiled.pdf";
-			exec("cd output; pdfunite $genDocList;");
+			exec("cd _temp; pdfunite $genDocList;");
 			header('Content-Type: application/binary');
 			header('Content-Disposition: attachment');
-			echo file_get_contents("output/$emis-compiled.pdf");
+			echo file_get_contents("_temp/$emis-compiled.pdf");
+			exec("rm _temp/$emis*");
 		}
 
 ?>

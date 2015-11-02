5939919d88bae2acee46fd53c6cbbd5f9197f434
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-03 01:32:49 +0545

    wkpdftohtml;

--- non-psql/index.php
+++ non-psql/index.php
@@ -52,7 +52,7 @@
 			$tocml = "<html><head></head><body><style>h4{display: inline-block; margin-left: 20px;}</style><h1>Table of Contents</h1>";
 			$tocFileName = $baseFileName."-toc";
 
-			$numPages = 1;
+			$numPages = 2;
 
 			var_dump($genDocList);
 
@@ -60,7 +60,7 @@
 				//exec("html-pdf output/$docName.html output/$docName.pdf");
 				exec('xvfb-run --server-args="-screen 0, 1366x768x24" wkhtmltopdf output/'.$docName.'.html _temp/'.$docName.'.pdf');
 				//EMIS250050003.html
-				//php -r 'echo exec("xvfb-run --server-args=\"-screen 0, 1366x768x24\" wkhtmltopdf output/EMIS210490003-C.html _temp/EMIS210490003-C.pdf");'
+				//php -r 'echo exec("xvfb-run --server-args=\"-screen 0, 1366x768x24\" --header-left=\"[webpage]\" --header-right=\"[page]/[toPage]\" --top 2cm --header-line wkhtmltopdf output/EMIS210490003-C.html _temp/EMIS210490003-C.pdf");'
 				$tocml .= "<div><h4>".preg_replace('/_/', '-', str_replace(".pdf", "", $docName))."</h4>  ................  <h4>".$numPages."</h4></div>";
 				$numPages += intval(exec("pdfinfo _temp/$docName.pdf | grep Pages | awk '{print $2}'"));
 			}

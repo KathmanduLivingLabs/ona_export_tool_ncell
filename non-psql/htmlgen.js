04fa08f675cdf0c06d9754eafd15c25793ef5e2e
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-05 15:25:25 +0545

    ok;

--- non-psql/htmlgen.js
+++ non-psql/htmlgen.js
@@ -19,13 +19,13 @@ _.map(data, function(item, index) {
 	}();
 	var pagetitle = function() {
 		if (emis.match(/EMIS[0-9]+-[a-z]+-[0-9]+/gi)) {
-			return "Building Element " + emis;
+			return "Building Element: " + emis;
 		}else{
-			return "Building" + emis;
+			return "School: " + emis;
 		}
 	}();
 
-	var html = '<html><head></head><body><style>.has-photo .value,img{text-align:center}#page-title{width:100%;margin-top:30px}#page-container{width:100%;padding-bottom:10px}#table-container{padding-bottom:20px}.table-row{line-height:1.8em;font-size:1.2em;font-family:Verdana;padding:0 8px;display:table-row}.key,.value{display:inline-block;display:table-cell;outline:#ccc solid thin;width:40%;padding:0 8px}.section-break{line-height:2em;margin-top:1em;width:81.6%}.section-name{padding:0 8px;margin-left:-2px;display:block;width:197.8%;font-weight:600;line-height:2.4em;margin-top:20px}img{display:block;height:20em}.has-photo .key,.has-photo .value{display:block;display:table-cell;width:81.26%}</style><div id="page-title"><h2>'+pagetitle+'</h2></div><div id="table-container"></div></body></html>';
+	var html = '<html><head></head><body><style>.has-photo .value,img{text-align:center}#page-title{width:100%;margin-top:30px}#page-container{width:100%;padding-bottom:10px}#table-container{padding-bottom:20px}.table-row{line-height:1.8em;font-size:1.2em;font-family:Verdana;padding:0 8px;display:table-row}.key,.value{display:inline-block;display:table-cell;outline:#ccc solid thin;width:40%;padding:0 8px}.section-break{line-height:2em;margin-top:1em;width:81.6%}.section-name{padding:0 8px;margin-left:-2px;display:block;width:197.8%;font-weight:600;line-height:2.4em;margin-top:20px}img{display:block;height:20em}.has-photo .key,.has-photo .value{display:block;display:table-cell;width:81.26%} .undefined{display:none;}</style><div id="page-title"><h2>'+pagetitle+'</h2></div><div id="table-container"></div></body></html>';
 
 	var page = $(html);
 	var container = page.find("#table-container");
@@ -50,9 +50,9 @@ _.map(data, function(item, index) {
 
 		var key = Object.keys(item_1)[0];
 
-		var tableRow = $("<tr class='table-row'></tr>").addClass(key);
-		tableRow.append($("<td class='key'></td>").text(item_1[key][0] + ""));
-		tableRow.append($("<td class='value'></td>").text(unescape(item_1[key][1])) + "");
+		var tableRow = $("<div class='table-row'></div>").addClass(key);
+		tableRow.append($("<div class='key'></div>").text(item_1[key][0] + ""));
+		tableRow.append($("<div class='value'></div>").text(unescape(item_1[key][1])) + "");
 		if(unescape(item_1[key][1])==="undefined"){
 			tableRow.find(".value").text("n/a");
 			tableRow.addClass("undefined");
@@ -60,7 +60,7 @@ _.map(data, function(item, index) {
 
 		if(unescape(item_1[key][1]).match(".jpg")){
 			tableRow.find(".value").append($("<img/>").attr({
-				src: "../"+unescape(item_1[key][1])
+				src: "../"+unescape(item_1[key][1]).split(".")[0]+"-large.jpg"
 			}));
 			tableRow.addClass("has-photo");
 		}

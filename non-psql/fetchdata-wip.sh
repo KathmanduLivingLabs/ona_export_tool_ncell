6cd67ce25fc058865c83d296f470426db41b6449
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-15 14:31:33 +0545

    fetchdatanew

---
+++ non-psql/fetchdata-wip.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+touch schools.json
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.json >schools.json
+done
+
+touch buildings.json
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.json >buildings.json
+done
+
+touch building_elements.json
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.json >building_elements.json
+done
+
+touch school.csv
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.csv >school.csv
+done
+
+touch building.csv
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.csv >building.csv
+done
+
+touch buildingelement.csv
+
+while [$(stat --printf="%s")=0]
+do
+	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.csv >buildingelement.csv
+done
+
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.json >schools.json
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.json >buildings.json
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.json >building_elements.json
+
+
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.csv >school.csv
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.csv >building.csv
+#curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.csv >buildingelement.csv
+
+
+
+

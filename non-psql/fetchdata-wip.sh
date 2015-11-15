2c5cc81106d4f514a1a6e81d66099f3362b77d2b
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-15 14:38:31 +0545

    fetchdata-wip.sh

--- non-psql/fetchdata-wip.sh
+++ non-psql/fetchdata-wip.sh
@@ -2,42 +2,42 @@
 
 touch schools.json
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" schools.json)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.json >schools.json
 done
 
 touch buildings.json
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" buildings.json)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.json >buildings.json
 done
 
 touch building_elements.json
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" building_elements.json)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.json >building_elements.json
 done
 
 touch school.csv
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" school.csv)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68589.csv >school.csv
 done
 
 touch building.csv
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" building.csv)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/68590.csv >building.csv
 done
 
 touch buildingelement.csv
 
-while [$(stat --printf="%s")=0]
+while [$(stat --printf="%s" buildingelement.csv)=0]
 do
 	curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/80364.csv >buildingelement.csv
 done

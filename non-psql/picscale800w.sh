6ddc796b8aefb4eede4ccdc91300503f947ebc89
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-18 10:06:43 +0545

    picscale bugfix

--- non-psql/picscale800w.sh
+++ non-psql/picscale800w.sh
@@ -3,7 +3,7 @@
 imgdimstr=$(identify -format "%wx%h" $1)
 imgdim=(${imgdimstr//x/ })
 #imgdim[0]=$((${imgdim[0]} / $1))
-ratio=$(imgdim[0]/800)
+ratio=$(${imgdim[0]}/800)
 imgdim[1]=$((${imgdim[1]} / $ratio))
 newdimstr="800x${imgdim[1]}"
 gm mogrify -size $newdimstr $1 -resize $newdimstr

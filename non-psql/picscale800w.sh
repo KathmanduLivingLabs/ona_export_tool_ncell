ee333e7d49739d28ae2e3d59c89677fe4841a219
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-18 22:56:40 -0500

    fetchphotos

--- non-psql/picscale800w.sh
+++ non-psql/picscale800w.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-imgdimstr=$(identify -format "%wx%h" $1)
-imgdim=(${imgdimstr//x/ })
-#imgdim[0]=$((${imgdim[0]} / $1))
-ratio=(10#${imgdim[0]}/800)
-imgdim[1]=(10#${imgdim[1]}/$ratio)
-newdimstr="800x${imgdim[1]}"
-gm mogrify -size $newdimstr $1 -resize $newdimstr
+#imgdimstr=$(identify -format "%wx%h" $1)
+#imgdim=(${imgdimstr//x/ })
+#####imgdim[0]=$((${imgdim[0]} / $1))
+#####echo ${imgdim[0]} | bc
+#ratio=$(bc <<< "scale=2; ${imgdim[0]}/800")
+#####echo $ratio
+#imgh=$(bc <<< "${imgdim[1]}/$ratio")
+#imgh=$(echo $imgh)
+#newdimstr="800x$imgh"
+####echo $newdimstr
+#gm mogrify -size $newdimstr $1 -resize $newdimstr
+gm mogrify -size 800x800 -resize 800x800 $1

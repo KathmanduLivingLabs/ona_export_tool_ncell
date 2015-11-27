509864602c45dced46227f6e68479c6e298644cd
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-27 13:14:41 -0500

    filterdatabyemis

--- non-psql/filter-data-by-emis.js
+++ non-psql/filter-data-by-emis.js
@@ -16,46 +16,49 @@ glob("./_temp/*_"+outputFileName, function(err, fileList) {
 	if (err) throw err;
 
 	outputFileName = fileList.length + "_" + outputFileName;
-
+var fileNameList=[];
 	data.forEach(function(item, index) {
-		var fileNameList = [];
+//		var fileNameList = [];
+JSON.stringify(item).match(new RegExp((process.argv[2]+"-\w+"), "i"))?console.log(JSON.stringify(item).match(new RegExp((process.argv[2]+"-\w+"), "i"))):1;
 		if (JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+-\d", "i"))) {
+			console.log(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+-\d", "i")));
 			if(filtered.length){
-			filtered.forEach(item_1, index_1){
+			filtered.forEach(function(item_1, index_1){
 				if (JSON.stringify(item_1).match(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+-\d+", "i"))[0])){
 					filtered[index_1].push(item);
 				}
-			}
+			});
 		}else{
 			filtered.push([item]);
 		}
 
-		fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+-\d+", "i"))[0]);
+		fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+-\d+", "i"))[0]+".json");
 
 		}else if(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+", "i"))){
+console.log(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+", "i")));
 
 			if(filtered.length){
-			filtered.forEach(item_1, index_1){
+			filtered.forEach(function(item_1, index_1){
 				if (JSON.stringify(item_1).match(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+", "i"))[0])){
 					filtered[index_1].push(item);
 				}
-			}
+			});
 		}else{
 			filtered.push([item]);
 		}
 
-		fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+", "i"))[0]);
+		fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2]+"-\w+", "i"))[0]+".json");
 
 		}else if(JSON.stringify(item).match(new RegExp(process.argv[2], "i"))){
 			filtered.push([item]);
-			fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2], "i"))[0]);
+			fileNameList.push(JSON.stringify(item).match(new RegExp(process.argv[2], "i"))[0]+".json");
 		}
 	});
 
 	
 
 	filtered.forEach(function(item, index){
-		fs.writeFileSync("./_temp/"+fileNameList[index]+, JSON.stringify(item));
+		fs.writeFileSync("./_temp/"+fileNameList[index], JSON.stringify(item));
 	});
 
 	//fs.writeFileSync("./_temp/"+outputFileName, JSON.stringify(filtered));

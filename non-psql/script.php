a0ffcca173e64a6a6ec96a992a49767d2c743301
Pratik Gautam <gautamxpratik@gmail.com>
2015-11-30 19:15:00 +0545

    csvonlymode

--- non-psql/script.php
+++ non-psql/script.php
@@ -6,6 +6,7 @@ $endDate = $_GET['enddate'];
 $tableName = $_GET['tablename'];
 $tableIDs = file_get_contents("tableIDs.json");
 $tableIDs = json_decode($tableIDs, true);
+$query = $_GET['query'];
 
 
 $clientAddr = $_SERVER['REMOTE_ADDR'];
@@ -23,9 +24,12 @@ exit();
 
 //&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $enddate, $results)
 if(preg_match("/(\d{4})-(\d{2})-(\d{2})/", $startDate, $results) 
-	&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results2) && $tableIDs[$tableName]){
+	&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results2) && $tableIDs[$tableName] && $query=='csvonly'){
 	echo explode(" ", exec('./ona-get-data-and-run-extractor.sh '.$startDate.' '.$endDate.' '.$tableName.' '.$tableIDs[$tableName]))[0];
-}else{
+}elseif(preg_match("/(\d{4})-(\d{2})-(\d{2})/", $startDate, $results) 
+	&& preg_match("/(\d{4})-(\d{2})-(\d{2})/", $endDate, $results2) && $tableIDs[$tableName]){
+		echo explode(" ", exec('./ona-get-data-and-run-extractor-csvonly.sh '.$startDate.' '.$endDate.' '.$tableName.' '.$tableIDs[$tableName]))[0];
+	}else{
 	echo "no";
 }
 

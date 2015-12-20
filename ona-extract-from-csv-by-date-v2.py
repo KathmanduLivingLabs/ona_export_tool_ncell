732a0531d450711ce3e8016969cafeb05b50c6c1
Pratik Gautam <gautamxpratik@gmail.com>
2015-12-20 18:21:41 +0545

    csv filter using python csv module;

---
+++ ona-extract-from-csv-by-date-v2.py
@@ -0,0 +1,51 @@
+import sys
+import csv
+import re
+
+datafile = open(sys.argv[3], 'r')
+
+startDate = sys.argv[1]
+endDate = sys.argv[2]
+
+zipList = ''
+
+
+data = csv.reader(datafile)
+
+headers = data.next()
+
+dateColumn = headers.index('_submission_time')
+emisColumn = headers.index('general_detail/emis/school_emis')
+
+filtered = [','.join(headers)]
+emis = ''
+
+def getFileNameRoot(name):
+        prefix = name.split('.')
+        prefix.pop()
+        '.'.join(prefix)
+        return prefix
+
+for item in data:
+        itemSubmissionDate = item[dateColumn].split('T',1)[0]
+        if itemSubmissionDate >= startDate and itemSubmissionDate <= endDate:
+                filtered.append(','.join(item))
+                emis += ', ' + item[emisColumn]
+                #zipList += " " + item.join(";").match(/(\d)+.jpg/gi).join(" ").replace(/.jpg/gi, "-large.jpg");
+                zipList += ' ' + re.compile('(\d+).jpg').sub('\\1-large.jpg',' '.join(re.compile('\d+.jpg', re.IGNORECASE).findall(';'.join(item))))
+        else:
+                item = []
+data = []
+
+output_filenameRoot = str(getFileNameRoot(sys.argv[3])) + '_' + startDate.replace('-', '_') + '_' + endDate.replace('-', '_') + '_records_' + str((len(filtered) - 1)) + '_pictures_' + str((len(zipList.split(' ')) - 2)) 
+
+zipList = output_filenameRoot + '.zip ' + output_filenameRoot + '.csv ' + zipList
+
+
+open(output_filenameRoot+'.csv', 'w').write('\n'.join(filtered))
+
+open('emis.txt', 'w').write(emis)
+
+print(zipList)
+
+

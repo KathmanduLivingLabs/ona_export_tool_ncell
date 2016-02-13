cb8fc165dbbfd767542ce090b2c6a28e9cc4703d
Pratik Gautam <gautamxpratik@gmail.com>
2016-02-13 08:39:58 -0500

    ona curl issue fix (work in progress);

--- fetchData-v2.sh
+++ fetchData-v2.sh
@@ -21,34 +21,34 @@ do
 	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/80364.json >building_elements_temp.json
 done
 
-touch school_temp.csv
+#touch school_temp.csv
 
-while [ $(stat --printf="%s" school_temp.csv) -eq 0 ]
-do
-	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/68589.csv >school_temp.csv
-done
+#while [ $(stat --printf="%s" school_temp.csv) -eq 0 ]
+#do
+#	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/68589.csv >school_temp.csv
+#done
 
-touch building_temp.csv
+#touch building_temp.csv
 
-while [ $(stat --printf="%s" building_temp.csv) -eq 0 ]
-do
-	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/68590.csv >building_temp.csv
-done
+#while [ $(stat --printf="%s" building_temp.csv) -eq 0 ]
+#do
+#	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/68590.csv >building_temp.csv
+#done
 
-touch buildingelement_temp.csv
+#touch buildingelement_temp.csv
 
-while [ $(stat --printf="%s" buildingelement_temp.csv) -eq 0 ]
-do
-	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/80364.csv >buildingelement_temp.csv
-done
+#while [ $(stat --printf="%s" buildingelement_temp.csv) -eq 0 ]
+#do
+#	curl -X GET -u "klltesting:klltesting" https://api.ona.io/api/v1/data/80364.csv >buildingelement_temp.csv
+#done
 
 mv schools_temp.json schools.json
 mv buildings_temp.json buildings.json
 mv building_elements_temp.json building_elements.json
 
-mv school_temp.csv school.csv
-mv building_temp.csv building.csv
-mv buildingelement_temp.csv buildingelement.csv
+#mv school_temp.csv school.csv
+#mv building_temp.csv building.csv
+#mv buildingelement_temp.csv buildingelement.csv
 
 ./fetchPhotos.sh
 

a353c74f7772692a6c2c48ad82df44673cf0bc06
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-26 16:14:35 +0545

    session check;

--- index.php
+++ index.php
@@ -1,22 +1,72 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header('Content-Type: text/plain');
 
 $clientAddr          = $_SERVER['REMOTE_ADDR'];
 $clientSessionCookie = $_COOKIE['session'];
 
-if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
-	$validSessionCookieForClient = file_get_contents($clientAddr.'.addr');
-	if (!(preg_match("/\w+/", $validSessionCookieForClient, $validCookieHash) && $clientSessionCookie == $validCookieHash[0])) {
-		echo "no";
-		exit();
-	}
+$requestURI = $_SERVER['REQUEST_URI'];
+
+if (preg_match("/auth.php/", $requestURI)) {
+	include_once "auth.php";
+	exit();
+}
+
+if (preg_match("/.php/", $requestURI)) {
+	include_once $requestURI;
+} else if (preg_match("/.jpg/", $requestURI) || preg_match("/.pdf/", $requestURI) || preg_match("/.csv/", $requestURI)) {
+	header("Pragma: public");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+	header("Content-Type: application/force-download");
+	header("Content-Type: application/octet-stream");
+	header("Content-Type: application/download");
+	header('Content-Length: '.filesize($requestURI));
+	header("Content-Disposition: attachment;filename=".$requestURI);
+	header("Content-Transfer-Encoding: binary ");
+} else if (preg_match("/.html/", $requestURI)) {
+	header('Content-Type: text/html');
+	echo file_get_contents($requestURI);
 } else {
+	header('Content-Type: text/plain');
 	echo "no";
 	exit();
 }
 
-var_dump($_SERVER);
+/*
+if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
+$validSessionCookieForClient = file_get_contents($clientAddr.'.addr');
+if (preg_match("/\w+/", $validSessionCookieForClient, $validCookieHash) && $clientSessionCookie == $validCookieHash[0]) {
+if (preg_match("/.php/", $requestURI)) {
+include_once $requestURI;
+} else if (preg_match("/.jpg/", $requestURI) || preg_match("/.pdf/", $requestURI) || preg_match("/.csv/", $requestURI)) {
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Content-Type: application/force-download");
+header("Content-Type: application/octet-stream");
+header("Content-Type: application/download");
+header('Content-Length: '.filesize($requestURI));
+header("Content-Disposition: attachment;filename=".$requestURI);
+header("Content-Transfer-Encoding: binary ");
+} else if (preg_match("/.html/", $requestURI)) {
+header('Content-Type: text/html');
+echo file_get_contents($requestURI);
+} else {
+header('Content-Type: text/plain');
+echo "no";
+exit();
+}
+} else {
+header('Content-Type: text/plain');
+echo "no";
+exit();
+}
+} else {
+header('Content-Type: text/plain');
+echo "no";
+exit();
+}
+ */
 
 ?>

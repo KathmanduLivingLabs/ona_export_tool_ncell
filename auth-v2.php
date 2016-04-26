f07bac9d92c81afad6d770e4108b1f07e96437f8
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-26 13:53:19 +0545

    session;

--- auth-v2.php
+++ auth-v2.php
@@ -6,12 +6,12 @@ header('Content-Type: text/plain');
 $authString1 = $_POST['auth1'];
 $authString2 = $_POST['auth2'];
 
-if(!($authString1 && $authString2)){
+if (!($authString1 && $authString2)) {
 	echo "false";
 	exit();
 }
 
-if(preg_match("/\W/", $authString1, $c)){
+if (preg_match("/\W/", $authString1, $c)) {
 	echo "false";
 	exit();
 }
@@ -26,21 +26,21 @@ $authString2Calc = exec("echo $authString2 | openssl enc -base64 -d | openssl en
 $clientAddr = $_SERVER['REMOTE_ADDR'];
 
 if ($authString1 == $validAuth1 && $authString2Calc == $validAuth2) {
-	if(preg_match("/\d+\.\d+\.\d+\.\d+/",$clientAddr, $result)){
-	$secret_word = exec("date")+rand();
-	$cookieString = $authString1.','.md5($validAuth1.$secret_word);
-    setcookie('login', $cookieString);
-    setcookie('surveyor_id',$surveyor_id);
+	if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
+		$secret_word  = exec("date")+rand();
+		$cookieString = $authString1.','.md5($validAuth1.$secret_word);
+		setcookie('session', $cookieString);
+		setcookie('surveyor_id', $surveyor_id);
 
-	    $logfile = fopen('authlog.log','a');
+		$logfile = fopen('authlog.log', 'a');
 		file_put_contents($clientAddr.'.addr', $cookieString);
-		fwrite($logfile,date("Y-m-d h:i:s/a T")."\t".$authString1."\t".$clientAddr."\n");
+		fwrite($logfile, date("Y-m-d h:i:s/a T")."\t".$authString1."\t".$clientAddr."\n");
 		fclose($logfile);
 		echo "true";
-	}else{
+	} else {
 		echo "false";
 	}
-}else{
+} else {
 	echo "false";
 }
 ?>

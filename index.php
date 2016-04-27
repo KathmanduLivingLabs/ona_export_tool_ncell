aa8ab3c619fce144687843a9b4168a41c9a9e836
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-27 16:41:18 +0545

    session check;

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ $clientAddr = $_SERVER['REMOTE_ADDR'];
 
 $clientSessionCookie = $_GET['key'];
 
-$requestURI = $_SERVER['REQUEST_URI'];
+$requestURI    = $_SERVER['REQUEST_URI'];
+$requestScript = $_SERVER['SCRIPT_NAME'];
 
 if (preg_match("/auth.php/", $requestURI)) {
 	include_once "auth.php";
@@ -42,7 +43,7 @@ if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
 	$validSessionCookieForClient = file_get_contents($clientAddr.'.addr');
 	if (preg_match("/\w+-\w+/", $validSessionCookieForClient, $validCookieHash) && $clientSessionCookie == $validCookieHash[0]) {
 		if (preg_match("/.php/", $requestURI)) {
-			include_once str_replace('/', '', $requestURI);
+			include_once '.'.$requestScript;
 		} else if (preg_match("/.jpg/", $requestURI) || preg_match("/.pdf/", $requestURI) || preg_match("/.csv/", $requestURI)) {
 			header("Pragma: public");
 			header("Expires: 0");
@@ -50,13 +51,13 @@ if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
 			header("Content-Type: application/force-download");
 			header("Content-Type: application/octet-stream");
 			header("Content-Type: application/download");
-			header('Content-Length: '.filesize(str_replace('/', '', $requestURI)));
+			header('Content-Length: '.filesize('.'.$requestURI));
 			header("Content-Disposition: attachment;filename=".str_replace('/', '', $requestURI));
 			header("Content-Transfer-Encoding: binary ");
-			echo file_get_contents(str_replace('/', '', $requestURI));
+			echo file_get_contents('.'.$requestURI);
 		} else if (preg_match("/.html/", $requestURI)) {
 			header('Content-Type: text/html');
-			echo file_get_contents(str_replace('/', '', $requestURI));
+			echo file_get_contents('.'.$requestURI);
 		} else {
 			header('Content-Type: text/plain');
 			echo "no";

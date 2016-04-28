003602344cf5ee73b0edc9f3b2070fb465afbf05
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-28 10:38:14 +0545

    positional parameters bugfix on ona-get-data-and-run-extractor-csvonly.sh and ona-get-data-and-run-extractor.sh;

--- ona-get-data-and-run-extractor.sh
+++ ona-get-data-and-run-extractor.sh
@@ -2,4 +2,4 @@
 from_date=$1
 to_date=$2
 #curl -X GET -u "wbsida321:KrS93r8Ttei63xN4ZB6rt0xLy" https://api.ona.io/api/v1/data/$4.csv >$3.csv
-./zipList.sh $from_date $to_date $3.csv $4
+./zipList.sh $from_date $to_date $3.csv $5

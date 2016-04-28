50ef84d236577d4319174a617fe4b1b662faa9b5
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-28 10:23:28 +0545

    surveyor_id on exported csv/zip;

--- ona-extract-from-csv-by-date-v2.py
+++ ona-extract-from-csv-by-date-v2.py
@@ -21,7 +21,7 @@ if(sys.argv[4] != '999'):
         ddata = []
         for item in data:
                 if item[0].upper() in sys.argv[4].upper():
-                        ddata.push(item)
+                        ddata.append(item)
         data = ddata
         ddata = 0
 
@@ -47,10 +47,12 @@ data = []
 
 output_filenameRoot = getFileNameRoot(sys.argv[3])[0] + '_' + startDate.replace('-', '_') + '_' + endDate.replace('-', '_') + '_records_' + str((len(filtered) - 1)) + '_pictures_' + str((len(zipList.split(' ')) - 2)) 
 
-zipList = output_filenameRoot + '.zip ' + output_filenameRoot + '.csv' + zipList
-
-
-open(output_filenameRoot+'.csv', 'w').write('\n'.join(filtered))
+if(sys.argv[4] == '999'):
+        zipList = output_filenameRoot + '.zip ' + output_filenameRoot + '.csv' + zipList
+        open(output_filenameRoot+'.csv', 'w').write('\n'.join(filtered))
+else:
+        zipList = output_filenameRoot +'_'+sys.argv[4]+ '.zip ' + output_filenameRoot +'_' + sys.argv[4] + '.csv' + zipList
+        open(output_filenameRoot+'_' + sys.argv[4]+'.csv', 'w').write('\n'.join(filtered))
 
 print(zipList)
 

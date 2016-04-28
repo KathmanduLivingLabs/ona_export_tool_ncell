00bc96ced47a46e1599765b841a713994aa24f86
Pratik Gautam <gautamxpratik@gmail.com>
2016-04-28 10:43:59 +0545

    surveyor_id on exported csv/zip;

--- zipList.sh
+++ zipList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-zipList=$(./ona-extract-from-csv-by-date-v2.py $1 $2 $3 $4) && zip $zipList>dump
+zipList=$(python ona-extract-from-csv-by-date-v2.py $1 $2 $3 $4) && zip $zipList>dump
 #zipList=$(python ona-extract-from-csv-by-date-v2.py $1 $2 $3) && zip $zipList>dump
 echo $zipList

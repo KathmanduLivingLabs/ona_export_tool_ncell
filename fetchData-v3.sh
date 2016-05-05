d7a5c4858f040ffbde85c4127bcc94a7cad1de84
Pratik Gautam <gautamxpratik@gmail.com>
2016-05-05 10:04:44 +0545

    csv according to surveyor_id;

--- fetchData-v3.sh
+++ fetchData-v3.sh
@@ -12,6 +12,11 @@ curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113422.json | ./slashslas
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113425.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/schools/dk.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113429.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/schools/bda.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113432.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/schools/pace.json
+
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113422.csv > schoolkd10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113425.csv > schooldk10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113429.csv > schoolbda30.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113432.csv > schoolpace12.csv
 ####
 
 ##buildings##
@@ -19,6 +24,11 @@ curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113423.json | ./slashslas
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113426.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/buildings/dk.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113430.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/buildings/bda.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113433.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/buildings/pace.json
+
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113423.csv > buildingkd10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113426.csv > buildingdk10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113430.csv > buildingbda30.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113433.csv > buildingpace12.csv
 ####
 
 ##building_elements##
@@ -26,6 +36,11 @@ curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113424.json | ./slashslas
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113427.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/building_elements/dk.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113431.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/building_elements/bda.json
 curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113434.json | ./slashslasher-for-wbsida-export-tool-v2.py > srcjson/building_elements/pace.json
+
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113424.csv > buildingelementkd10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113427.csv > buildingelementdk10.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113431.csv > buildingelementbda30.csv
+curl -X GET -u $onaAuth https://api.ona.io/api/v1/data/113434.csv > buildingelementpace12.csv
 ####
 
 

c16abe0295a09034e1c4fd23269fefe4d4afdd9e
Pratik Gautam <gautamxpratik@gmail.com>
2016-05-13 14:41:55 +0545

    bundled zip created in downloads folder for download;

--- zipList.sh
+++ zipList.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir downloads
 mkdir downloads/$5
-zipList=$(python ona-extract-from-csv-by-date-v2.py $1 $2 $3 $4) && zip "downloads/$5/$zipList">dump
-#zipList=$(python ona-extract-from-csv-by-date-v2.py $1 $2 $3) && zip $zipList>dump
+zipList=$(python ona-extract-from-csv-by-date-v2.py $1 $2 $3 $4) && zip downloads/$5/$zipList>dump
 echo $zipList

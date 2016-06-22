3058d1bf00b2ce9088ee062630faf5f0a8f7d062
Pratik Gautam <gautamxpratik@gmail.com>
2016-06-22 16:01:32 +0545

    cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+## Get into Current Dir to call python file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 mkdir _archives
 mv *.addr _archives/
 rm _temp/EMIS*
 rm output/*
 rm -rf downloads/*
-
-
-
-
 
 
 

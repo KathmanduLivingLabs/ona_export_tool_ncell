07d0a8e594b88c3a75741fe77c5564f413c07105
pujan789556 <pujan789556@gmail.com>
2016-08-04 14:59:47 +0545

    No authorization required

--- auth.php
+++ auth.php
@@ -3,44 +3,5 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/plain');
 
-$authString1 = $_POST['auth1'];
-$authString2 = $_POST['auth2'];
-
-if (!($authString1 && $authString2)) {
-	echo "false";
-	exit();
-}
-
-if (preg_match("/\W/", $authString1, $c)) {
-	echo "false";
-	exit();
-}
-
-$validAuth1 = exec("./authgen1.sh $authString1");
-$validAuth2 = exec("./authgen2.sh $validAuth1");
-
-$surveyor_id = exec("./getSurveyorId.sh $validAuth1");
-
-$authString2Calc = exec("echo $authString2 | openssl enc -base64 -d | openssl enc -aes-256-cbc -k $validAuth2 -d");
-
-$clientAddr = $_SERVER['REMOTE_ADDR'];
-
-if ($authString1 == $validAuth1 && $authString2Calc == $validAuth2) {
-	if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
-		$secret_word  = exec("date")+rand();
-		$cookieString = $authString1.'-'.md5($validAuth1.$secret_word);
-		//setcookie('session', $cookieString);
-		//setcookie('surveyor_id', $surveyor_id);
-
-		$logfile = fopen('authlog.log', 'a');
-		file_put_contents($clientAddr.'.addr', $cookieString);
-		fwrite($logfile, date("Y-m-d h:i:s/a T")."\t".$authString1."\t".$clientAddr."\n");
-		fclose($logfile);
-		echo '{"authorized":true,"session":{"surveyor_id":"'.$surveyor_id.'","key":"'.$cookieString.'"}}';
-	} else {
-		echo "false";
-	}
-} else {
-	echo "false";
-}
+echo '{"authorized":true,"session":{"surveyor_id":"bda30","key":"abcd-abcd1234"}}';
 ?>
--- index.php
+++ index.php
@@ -2,106 +2,13 @@
 
 header("Access-Control-Allow-Origin: *");
 
-$clientAddr = $_SERVER['REMOTE_ADDR'];
-//$clientSessionCookie = $_COOKIE['session'];
-
-$clientSessionCookie = $_GET['key'];
-
-$requestURI    = $_SERVER['REQUEST_URI'];
 $requestScript = $_SERVER['SCRIPT_NAME'];
 
 if (preg_match("/auth.php/", $requestURI)) {
 	include_once "auth.php";
 	exit();
 }
-
-
-if(!$clientSessionCookie){
-	header('Content-Type: text/plain');
-	echo "no";
-	exit();
-}
-
-if(!(preg_match("/.jpg/", $requestURI) || preg_match("/.pdf/", $requestURI) || preg_match("/.zip/", $requestURI) || preg_match("/.csv/", $requestURI))){
-
-	$surveyorID = $_GET['surveyor_id'];
-
-	if(!$surveyorID){
-		$surveyorID='';
-	}
-
-	$validAuth1 = explode('-',$clientSessionCookie)[0];
-
-	$validSurveyorID = exec("./getSurveyorId.sh $validAuth1");
-
-	if($surveyorID != $validSurveyorID){
-		header('Content-Type: text/plain');
-		echo "no";
-		exit();
-	}
-
-}
-
-/*
-if (preg_match("/.php/", $requestURI)) {
-include_once $requestURI;
-} else if (preg_match("/.jpg/", $requestURI) || preg_match("/.pdf/", $requestURI) || preg_match("/.csv/", $requestURI)) {
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Content-Type: application/force-download");
-header("Content-Type: application/octet-stream");
-header("Content-Type: application/download");
-header('Content-Length: '.filesize(str_replace('/', '', $requestURI)));
-header("Content-Disposition: attachment;filename=".str_replace('/', '', $requestURI));
-header("Content-Transfer-Encoding: binary ");
-echo file_get_contents(str_replace('/', '', $requestURI));
-} else if (preg_match("/.html/", $requestURI)) {
-header('Content-Type: text/html');
-echo file_get_contents(str_replace('/', '', $requestURI));
-} else {
-header('Content-Type: text/plain');
-echo "no";
-exit();
-}
- */
-
-if (preg_match("/\d+\.\d+\.\d+\.\d+/", $clientAddr, $result)) {
-	$validSessionCookieForClient = file_get_contents($clientAddr.'.addr');
-	if (preg_match("/\w+-\w+/", $validSessionCookieForClient, $validCookieHash) && $clientSessionCookie == $validCookieHash[0]) {
-		if (preg_match("/.php/", $requestURI)) {
-			include_once '.'.$requestScript;
-		} else if (preg_match("/.jpg/", $requestURI)) {
-			header('Content-Type: image/jpeg');
-			echo file_get_contents('.'.$requestScript);
-		} else if (preg_match("/.pdf/", $requestURI) || preg_match("/.zip/", $requestURI) || preg_match("/.csv/", $requestURI)) {
-			header("Pragma: public");
-			header("Expires: 0");
-			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-			header("Content-Type: application/force-download");
-			header("Content-Type: application/octet-stream");
-			header("Content-Type: application/download");
-			header('Content-Length: '.filesize('.'.$requestScript));
-			header("Content-Disposition: attachment;filename=".str_replace('/', '', $requestScript));
-			header("Content-Transfer-Encoding: binary ");
-			echo file_get_contents('.'.$requestScript);
-		} else if (preg_match("/.html/", $requestURI)) {
-			header('Content-Type: text/html');
-			echo file_get_contents('.'.$requestScript);
-		} else {
-			header('Content-Type: text/plain');
-			echo "no";
-			exit();
-		}
-	} else {
-		header('Content-Type: text/plain');
-		echo "no";
-		exit();
-	}
-} else {
-	header('Content-Type: text/plain');
-	echo "no";
-	exit();
-}
+include_once '.'.$requestScript;
+	
 
 ?>

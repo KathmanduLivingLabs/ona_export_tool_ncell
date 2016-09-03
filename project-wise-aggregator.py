25cd651782fa7ba64c2810fb3a4e8e05977a952e
Pratik Gautam <gautamxpratik@gmail.com>
2016-09-03 19:18:06 +0545

    sum;

---
+++ project-wise-aggregator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+dictListData = None
+
+with open sys.stdin as jsonFile:
+    dictListData = json.loads(jsonFile)
+    jsonFile.close()
+
+aggSumDict = {}
+
+for item in dictListData:
+    for k,v in item.items():
+        try:
+            if k in aggSumDict:
+                aggSumDict[k] += float(v)
+            else:
+                aggSumDict[k] = float(v)
+        except ValueError as e:
+            pass
+            
+print(json.dumps(aggSumDict))
+
+
+
+
+
+
